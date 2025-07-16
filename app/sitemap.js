1fb1381ae95e346c3b683f343fdc8d0486d8cfc3
Francesco Merighi <francescomerighi61@gmail.com>
2025-07-16 21:04:01 +0200

    Fix temporaneo per deploy

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,14 +1,5 @@
-import { categoryNames } from "@/lib/categories";
-
 export default async function sitemap() {
   const baseUrl = process.env.BASE_URL;
-  
-  const categories = Object.keys(categoryNames).map(category => ({
-    url: `${baseUrl}/${category}`,
-    lastModified: new Date(),
-    changeFrequency: 'monthly',
-    priority: 0.8
-  }));
 
   const staticPages = [
     {
