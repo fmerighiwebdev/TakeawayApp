021627df110812eb6dcaade9a27d09691d59c195
Francesco Merighi <51082657+fmerighiwebdev@users.noreply.github.com>
2026-02-03 23:33:20 +0100

    Banner fixes

--- components/cookie-banner.jsx
+++ components/cookie-banner.jsx
@@ -15,15 +15,15 @@ export default function CookieBanner({ slug }) {
 
   return (
     <>
-      <div className="fixed bottom-5 left-5 bg-white z-999 p-4 shadow-lg rounded-lg w-full max-w-sm flex flex-col gap-4">
-        <p>
+      <div className="fixed  bottom-0 sm:bottom-5 left-1/2 -translate-x-1/2 bg-white z-999 p-4 shadow-lg rounded-lg w-full max-w-md flex flex-col gap-4">
+        <p className="text-sm">
           Utilizziamo solo cookie tecnici necessari al corretto funzionamento
           dell’app. Non utilizziamo cookie di profilazione o tracciamento.
         </p>
 
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 justify-center">
           <button
-            className="bg-primary px-4 py-1 w-30 rounded-md shadow-lg text-primary-foreground"
+            className="text-sm bg-primary px-4 py-1 w-30 rounded-md shadow-lg text-primary-foreground"
             onClick={() => {
               setCookie(`cookieConsent-${slug}`, "rejected");
               setVisible(false);
@@ -33,7 +33,7 @@ export default function CookieBanner({ slug }) {
           </button>
 
           <button
-            className="bg-primary px-4 py-1 w-30 rounded-md shadow-lg text-primary-foreground"
+            className="text-sm bg-primary px-4 py-1 w-30 rounded-md shadow-lg text-primary-foreground"
             onClick={() => {
               setCookie(`cookieConsent-${slug}`, "accepted");
               setVisible(false);
